4484c75a8e0114a923266496d9fc091400632e27
rrekapalli <rekapallichaitanya@gmail.com>
2025-07-28 07:51:28 +0530

    stocks api updated in front end

---
+++ moneyplant-app/src/main/resources/static/app.js
@@ -0,0 +1,4 @@
+console.log('Money Plant application placeholder');
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Money Plant application loaded - actual application has not loaded');
+});
