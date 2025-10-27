50e3bba795a8ec2a2b3cdafd0f466b19364da1bd
rrekapalli <rekapallichaitanya@gmail.com>
2025-10-27 15:48:26 +0530

    Criteria builder removed as kiro is not able to build according to specs

--- frontend/projects/criteria-builder/src/lib/components/criteria-popover/tabs/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './fields-tab.component';
-export * from './operators-tab.component';
-export * from './functions-tab.component';
--- frontend/projects/criteria-builder/src/lib/components/undo-notification/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './undo-notification.component';
